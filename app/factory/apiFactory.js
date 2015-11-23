879f0207b72b1b41b27d21aa7b53a7715785ef4e
Pelle <pellebranden@me.com>
2015-11-23 15:33:07 +0100

    Added loading time and response size

--- app/factory/apiFactory.js
+++ app/factory/apiFactory.js
@@ -4,11 +4,15 @@ app.factory('apiFactory', function ($http, quilt4Config) {
 	
 	function apiGet(methodName, success, error) {
 				
+		var start = new Date();
+		
 		//TODO: Handle auth
 		$http.get(quilt4Config.apiBaseUrl + methodName)
 		
 		.success(function(response)	{
 			
+			var size = sizeof(response);
+			console.log((new Date() - start) + " ms: " + size + " bytes");
 			console.log(response);
 			success(response);		
 				
@@ -25,11 +29,15 @@ app.factory('apiFactory', function ($http, quilt4Config) {
 	
 	function apiPost(methodName, data, success, error) {
 				
+		var start = new Date();
+				
 		//TODO: Handle auth
 		$http.post(quilt4Config.apiBaseUrl + methodName, data)
 		
 		.success(function(response)	{
 			
+			var size = sizeof(response);
+			console.log((new Date() - start) + " ms: " + size + " bytes");			
 			console.log(response);
 			success(response);		
 				
