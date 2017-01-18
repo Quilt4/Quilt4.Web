d42ee3797e2e29cc1ba6353e1aa148c15134f798
Aquerr <nerdianin@gmail.com>
2017-01-18 13:30:42 +0100

    Added testing libraries for angular

--- src/Quilt4.Web/wwwroot/systemjs.config.js
+++ src/Quilt4.Web/wwwroot/systemjs.config.js
@@ -12,6 +12,16 @@
         map: {
             // our app is within the app folder
             app: 'app',
+            //Testing libraries
+            '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
+            '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
+            '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
+            '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+            '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+            '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
+            '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
+            '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
+
             // angular bundles
             '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
             '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
@@ -21,9 +31,10 @@
             '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
             '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
             '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
             // other libraries
             'rxjs': 'npm:rxjs',
-            'angular2-in-memory-web-api': 'npm:angular2-in-memory-web-api',
+            'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
         },
         // packages tells the System loader how to load when no filename and/or no extension
         packages: {
@@ -32,10 +43,6 @@
                 defaultExtension: 'js'
             },
             rxjs: {
-                defaultExtension: 'js'
-            },
-            'angular2-in-memory-web-api': {
-                main: './index.js',
                 defaultExtension: 'js'
             }
         }
